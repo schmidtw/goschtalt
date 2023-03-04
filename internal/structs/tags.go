3ad0be106883e3ffb5fa9ba8352c9eca817d20ae
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2023-03-04 12:49:39 -0800

    feat:include fatih/structs for adapters
    The upstream repo is archived, but the code is a great place to start
    for the translation needs goschtalt has.  Bring in the code from the
    following hash: 878a968ab22548362a09bdb3322f98b00f470d46

---
+++ internal/structs/tags.go
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2014 https://github.com/fatih
+// SPDX-FileCopyrightText: 2017 https://github.com/emret
+// SPDX-License-Identifier: MIT
+//
+// This file originated from https://github.com/fatih/structs/blob/878a968ab22548362a09bdb3322f98b00f470d46/tags.go
+
+package structs
+
+import "strings"
+
+// tagOptions contains a slice of tag options
+type tagOptions []string
+
+// Has returns true if the given option is available in tagOptions
+func (t tagOptions) Has(opt string) bool {
+	for _, tagOpt := range t {
+		if tagOpt == opt {
+			return true
+		}
+	}
+
+	return false
+}
+
+// parseTag splits a struct field's tag into its name and a list of options
+// which comes after a name. A tag is in the form of: "name,option1,option2".
+// The name can be neglectected.
+func parseTag(tag string) (string, tagOptions) {
+	// tag is one of followings:
+	// ""
+	// "name"
+	// "name,opt"
+	// "name,opt,opt2"
+	// ",opt"
+
+	res := strings.Split(tag, ",")
+	return res[0], res[1:]
+}
