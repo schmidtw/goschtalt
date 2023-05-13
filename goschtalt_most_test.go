c1c97ad2c5a387fa151323e8883a2e80ca627b8a
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2023-05-12 19:08:30 -0700

    Use the build tags correctly.

--- goschtalt_most_test.go
+++ goschtalt_most_test.go
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2022-2023 Weston Schmidt <weston_schmidt@alumni.purdue.edu>
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build !windows && !android
+
 package goschtalt
 
 import (
