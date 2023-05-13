4526aa404a8e649488b37c1eedcaa18c47693f8d
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2023-05-12 19:24:02 -0700

    Pass in nil for the array.

--- goschtalt_windows_test.go
+++ goschtalt_windows_test.go
@@ -22,7 +22,7 @@ func TestStdCfgLayoutWin(t *testing.T) {
 func TestCompileWin(t *testing.T) {
 	assert := assert.New(t)
 
-	_, err := New(AutoCompile(), stdCfgLayout("name"))
+	_, err := New(AutoCompile(), stdCfgLayout("name", nil))
 
 	assert.Error(err)
 }
