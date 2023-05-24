bcf4af8c9f1018f50d4ded7a03dbac7be3904f96
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2023-05-24 02:43:00 -0700

    Improve the number adapter quality and consistency quite a bit.

--- pkg/adapter/float.go
+++ pkg/adapter/float.go
@@ -4,82 +4,24 @@
 package adapter
 
 import (
-	"reflect"
-	"strconv"
-
 	"github.com/goschtalt/goschtalt"
 )
 
 // FloatUnmarshal converts a string to a float32/float64 or *float32/*float64 if
 // possible, or returns an error indicating the failure.
 func FloatUnmarshal() goschtalt.UnmarshalOption {
-	return goschtalt.AdaptFromCfg(marshalFloat{}, "FloatUnmarshal")
+	return goschtalt.AdaptFromCfg(
+		marshalNumber{
+			typ: "float",
+		},
+		"FloatUnmarshal")
 }
 
 // MarshalFloat converts a float32/float64 into its string configuration form.
 func MarshalFloat() goschtalt.ValueOption {
-	return goschtalt.AdaptToCfg(marshalFloat{}, "MarshalFloat")
-}
-
-type marshalFloat struct{}
-
-func (marshalFloat) From(from, to reflect.Value) (any, error) {
-	if from.Kind() != reflect.String {
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	ptr := false
-	bits := 32
-	switch to.Type() {
-	case reflect.TypeOf(float32(0.0)):
-	case reflect.TypeOf(new(float32)):
-		ptr = true
-	case reflect.TypeOf(float64(0.0)):
-		bits = 64
-	case reflect.TypeOf(new(float64)):
-		bits = 64
-		ptr = true
-	default:
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	rv64, err := strconv.ParseFloat(from.Interface().(string), bits)
-	if err != nil {
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	if bits == 64 {
-		if ptr {
-			return &rv64, nil
-		}
-		return rv64, nil
-	}
-
-	rv32 := float32(rv64)
-	if ptr {
-		return &rv32, nil
-	}
-	return rv32, nil
-}
-
-func (marshalFloat) To(from reflect.Value) (any, error) {
-	var f float64
-	bits := 32
-
-	switch from.Type() {
-	case reflect.TypeOf(float32(0.0)):
-		f = float64(from.Interface().(float32))
-	case reflect.TypeOf(float64(0.0)):
-		f = from.Interface().(float64)
-		bits = 64
-	case reflect.TypeOf(new(float32)):
-		f = float64(*(from.Interface().(*float32)))
-	case reflect.TypeOf(new(float64)):
-		f = *(from.Interface().(*float64))
-		bits = 64
-	default:
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	return strconv.FormatFloat(f, 'f', -1, bits), nil
+	return goschtalt.AdaptToCfg(
+		marshalNumber{
+			typ: "float",
+		},
+		"MarshalFloat")
 }
--- pkg/adapter/int.go
+++ pkg/adapter/int.go
@@ -4,126 +4,26 @@
 package adapter
 
 import (
-	"reflect"
-	"strconv"
-
 	"github.com/goschtalt/goschtalt"
 )
 
-// IntUnmarshal converts a string to a float32/float64 or *float32/*float64 if
-// possible, or returns an error indicating the failure.
+// IntUnmarshal converts a string to a int/int8/int16/int32/int64 or a pointer
+// version of them if possible, or returns an error indicating the failure.  Up
+// to triple pointers are supported.
 func IntUnmarshal() goschtalt.UnmarshalOption {
-	return goschtalt.AdaptFromCfg(marshalInt{}, "IntUnmarshal")
+	return goschtalt.AdaptFromCfg(
+		marshalNumber{
+			typ: "int",
+		},
+		"IntUnmarshal")
 }
 
-// MarshalInt converts a float32/float64 into its string configuration form.
+// MarshalInt converts a int/int8/int16/int32/int64 into its string
+// configuration form.
 func MarshalInt() goschtalt.ValueOption {
-	return goschtalt.AdaptToCfg(marshalInt{}, "MarshalInt")
-}
-
-type marshalInt struct{}
-
-const intSize = 32 << (^uint(0) >> 32 & 1) // 32 or 64
-
-func (marshalInt) From(from, to reflect.Value) (any, error) { //nolint:funlen
-	if from.Kind() != reflect.String {
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	var (
-		ptr     bool
-		intType bool
-		bits    int
-	)
-
-	switch to.Type() {
-	case reflect.TypeOf(int(0)):
-		bits = intSize
-		intType = true
-	case reflect.TypeOf(int8(0)):
-		bits = 8
-	case reflect.TypeOf(int16(0)):
-		bits = 16
-	case reflect.TypeOf(int32(0)):
-		bits = 32
-	case reflect.TypeOf(int64(0)):
-		bits = 64
-	case reflect.TypeOf(new(int)):
-		ptr = true
-		bits = intSize
-		intType = true
-	case reflect.TypeOf(new(int8)):
-		ptr = true
-		bits = 8
-	case reflect.TypeOf(new(int16)):
-		ptr = true
-		bits = 16
-	case reflect.TypeOf(new(int32)):
-		ptr = true
-		bits = 32
-	case reflect.TypeOf(new(int64)):
-		ptr = true
-		bits = 64
-	default:
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	num, err := strconv.ParseInt(from.Interface().(string), 0, bits)
-	if err != nil && err.(*strconv.NumError).Err == strconv.ErrSyntax { //nolint:errorlint
-		return nil, goschtalt.ErrNotApplicable
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	if bits == 8 {
-		rv := int8(num)
-		if ptr {
-			return &rv, nil
-		}
-		return rv, nil
-	}
-
-	if bits == 16 {
-		rv := int16(num)
-		if ptr {
-			return &rv, nil
-		}
-		return rv, nil
-	}
-
-	if intType {
-		rv := int(num)
-		if ptr {
-			return &rv, nil
-		}
-		return rv, nil
-	}
-
-	if bits == 32 {
-		rv := int32(num)
-		if ptr {
-			return &rv, nil
-		}
-		return rv, nil
-	}
-
-	if ptr {
-		return &num, nil
-	}
-	return num, nil
-}
-
-func (m marshalInt) To(from reflect.Value) (any, error) {
-	if from.CanInt() {
-		return strconv.FormatInt(from.Int(), 10), nil
-	}
-
-	if from.Kind() != reflect.Pointer {
-		return nil, goschtalt.ErrNotApplicable
-	}
-
-	from = from.Elem()
-
-	return m.To(from)
+	return goschtalt.AdaptToCfg(
+		marshalNumber{
+			typ: "int",
+		},
+		"MarshalInt")
 }
