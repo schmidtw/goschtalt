e78061c0b31e21a0d9d7dea6f9cf6a008dc442ab
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2025-01-23 20:34:48 -0800

    Add copyright headers and remove md files.

--- internal/mapstructure/mapstructure.go
+++ internal/mapstructure/mapstructure.go
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2013-2025 Mitchell Hashimoto
+// SPDX-FileCopyrightText: 2025 Weston Schmidt
+// SPDX-License-Identifier: MIT
+//
+// This file originated from https://github.com/mitchellh/mapstructure
+//
 // Package mapstructure exposes functionality to convert one arbitrary
 // Go type into another, typically to convert a map[string]interface{}
 // into a native Go structure.
--- internal/mapstructure/mapstructure_bugs_test.go
+++ internal/mapstructure/mapstructure_bugs_test.go
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2013-2025 Mitchell Hashimoto
+// SPDX-FileCopyrightText: 2025 Weston Schmidt
+// SPDX-License-Identifier: MIT
+//
+// This file originated from https://github.com/mitchellh/mapstructure
+
 package mapstructure
 
 import (
